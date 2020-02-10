6400785e4d9747d0855000877fc3aa902e4a0362
Lanph3re <dhkvmfzld@gmail.com>
2020-02-10 17:08:33 +0900

    rename all directories

--- arp_spoof/include/session.h
+++
@@ -1,9 +0,0 @@
-#ifndef SESSION_H_
-#define SESSION_H_
-
-#include "arpspoof.h"
-
-void
-ArpSpoof(const char* ifname, const char* sender_ip, const char* target_ip);
-
-#endif
--- sum_test/sum.cpp
+++
@@ -1,5 +0,0 @@
-#include "sum.h"
-
-int sum(int n) {
-  return (n + 1)*n/2;
-}
